78acba2bfc3776789f0cba9784ce8647e055fe7b
Elisabetta Agnello <elisabetta24.ea@gmail.com>
2025-12-10 14:25:53 +0100

    Update generate_library.py

--- scripts/generate_library.py
+++ scripts/generate_library.py
@@ -23,7 +23,7 @@ card_template = """
     </div>
     <a href="pdfs/{filename}" class="download-btn" target="_blank">
         <svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/></svg>
-        Scarica Appunti
+        View/Download
     </a>
 </div>
 """
